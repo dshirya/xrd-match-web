673b8faeaca244eebce12e5e9d0b54ce94a94e84
Danila Shiryaev <dshirya@gmail.com>
2025-03-28 12:56:52 -0400

    update

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from layout import app
-import callbacks  # This registers all callbacks with the app
+from dash import Dash
+from layout import get_layout
+from callbacks import register_callbacks
+
+# Initialize the Dash app here.
+app = Dash(__name__)
+server = app.server
+
+# Set the layout from layout.py.
+app.layout = get_layout()
+
+# Register all callbacks using the app instance.
+register_callbacks(app)
 
 if __name__ == "__main__":
     app.run(debug=True, port=8050)
